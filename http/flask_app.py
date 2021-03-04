91a2f8bb10958fe448147a358a58a4b149da2fae
Ivanm <ivanm2114@mail.ru>
2021-03-04 18:07:23 +0300

    opening site added

--- http/flask_app.py
+++ http/flask_app.py
@@ -389,5 +389,10 @@ def preparement(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<profession>')
+def training_image(profession):
+    return render_template('training_scheme.html', profession=profession.lower())
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
