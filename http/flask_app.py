6298c54807cf0077763725209b182a6e03b89370
Ivanm <ivanm2114@mail.ru>
2021-03-11 18:03:39 +0300

    all tasks reworked

--- http/flask_app.py
+++ http/flask_app.py
@@ -1,8 +1,20 @@
 from flask import Flask, request, render_template
 from flask import url_for
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
 
 @app.route('/')
 def f():
@@ -300,6 +312,14 @@ def training_image(profession):
 def profs_list(mode):
     profs = ['Пилот', 'Криптобиолог', 'Грузчик', 'Врач']
     return render_template('list_prof.html', profs=profs, mode=mode)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('auth.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
