16145c979b7d57ccc1b9820964ddbbf06db86f11
ivanm2114 <ivanm2114@mail.ru>
2021-03-16 19:20:57 +0300

    distribution added

--- http/flask_app.py
+++ http/flask_app.py
@@ -360,6 +360,10 @@ def login():
 def success():
     return render_template('success.html')
 
+@app.route('/distribution')
+def distribution():
+    return render_template('distribution.html')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
