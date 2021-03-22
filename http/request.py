5ad70f4cef1555011055b377e0eb69e5c229954c
ivanm2114 <ivanm2114@mail.ru>
2021-03-22 23:07:36 +0300

    request2

--- http/request.py
+++ http/request.py
@@ -3,6 +3,7 @@ from data import db_session
 from data.users import User
 from data.jobs import Jobs
 
+
 def main(db_name):
     db_session.global_init(db_name)
     db_sess = db_session.create_session()
