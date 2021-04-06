7304b9891b205348574335248ea2075e4d10248b
Ivanm <ivanm2114@mail.ru>
2021-04-06 13:22:09 +0300

    connecting database and categories working test

--- http/test.py
+++ http/test.py
@@ -17,10 +17,10 @@ from forms.departments import CreateDepartment, EditDepartment
 db_session.global_init('db/mars_explorer.db')
 db_sess = db_session.create_session()
 
-cat = db_sess.query(Category).filter(Category.id == 4).first()
+cat = db_sess.query(Category).filter(Category.id == 3).first()
 job = db_sess.query(Jobs).filter(Jobs.id == 3).first()
 job.categories.remove(cat)
-cat = db_sess.query(Category).filter(Category.id == 3).first()
+cat = db_sess.query(Category).filter(Category.id == 4).first()
 job.categories.append(cat)
 
 db_sess.commit()
